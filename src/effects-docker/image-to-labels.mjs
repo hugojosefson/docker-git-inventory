fe03699c3919de93d8ce73f911808da150a8d835
Hugo Josefson <hugo@josefson.org>
2020-06-09 14:53:04 +0200

    More labels.

--- src/effects-docker/image-to-labels.mjs
+++ src/effects-docker/image-to-labels.mjs
@@ -1,10 +1,14 @@
 import docker from '../effects/docker.mjs'
 import noop from '../fn/noop.mjs'
 
-export default ({ image, ...rest }) =>
+export default ({ image, labels = {}, ...rest }) =>
   docker(['pull', '-q', image])
     .errors(noop)
     .flatMap(() =>
-      docker(['inspect', image, '--format', '{{json .Config.Labels}}'])
+      docker(['inspect', image, '--format', '{{json .ContainerConfig.Labels}}'])
     )
-    .map(labels => ({ image, ...rest, labels }))
+    .map(moreLabels => ({
+      image,
+      ...rest,
+      labels: { ...labels, ...moreLabels },
+    }))
--- src/effects-docker/service-to-labels.mjs
+++ src/effects-docker/service-to-labels.mjs
@@ -1,9 +1,11 @@
 import docker from '../effects/docker.mjs'
 
-export default ({ serviceId, ...rest }) =>
-  docker([
-    'inspect',
-    serviceId,
-    '--format',
-    '{{json .Spec.Labels}}',
-  ]).map(labels => ({ serviceId, ...rest, labels }))
+export default ({ serviceId, labels = {}, ...rest }) =>
+  docker(['inspect', serviceId, '--format', '{{json .Spec.Labels}}']).map(
+    moreLabels => ({
+      serviceId,
+      ...rest,
+      labels: { ...labels, ...moreLabels },
+    })
+  )
+// TODO: search all of the inspection json output for Labels, in all inspections
