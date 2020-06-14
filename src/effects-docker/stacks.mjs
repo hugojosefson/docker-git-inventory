830805cc9d108d95727327bf75628ac6acd485a7
Hugo Josefson <hugo@josefson.org>
2020-06-14 20:35:44 +0200

    Don't fail on no stacks.

--- src/effects-docker/stacks.mjs
+++ src/effects-docker/stacks.mjs
@@ -1,3 +1,4 @@
 import docker from '../effects/docker.mjs'
 
-export default () => docker(['stack', 'ls', '--format', '{{json .Name}}'])
+export default () =>
+  docker(['stack', 'ls', '--format', '{{json .Name}}']).filter(name => name)
