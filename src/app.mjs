7a7bc70a0436b3d2aaa832b261b4e03204429442
Hugo Josefson <hugo@josefson.org>
2020-06-14 15:47:05 +0000

    Remove unused args.

--- src/app.mjs
+++ src/app.mjs
@@ -8,15 +8,9 @@ import rootHandlers from './path-handlers/root-handlers.mjs'
  * HTTP API for working with `docker-inventory-git`.
  * @param {object} options
  * @param {Express|Application|IRouter} options.app Express.js Application instance or Router on which to apply paths.
- * @param {GitAuth} options.gitAuthentication Response for our `AuthCallback` to `onAuth`. If `undefined`, `onAuth` will not be used.
- * @param {AuthCallback} options.onAuth `onAuth` implementation.
  * @returns {Express|Application|IRouter} app, mutated.
  */
-export default ({
-  app = express(),
-  gitAuthentication = undefined,
-  onAuth = gitAuthentication ? () => gitAuthentication : undefined,
-} = {}) => {
+export default ({ app = express() } = {}) => {
   const { registerPath, getDocumentation } = RegisterPath(app)
 
   registerPath('/inventory', inventoryHandlers())
